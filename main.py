8ca039a6b12724a0682f61216060665a0e754c84
FlorianCasse <68942132+FlorianCasse@users.noreply.github.com>
2021-05-24 11:50:11 +0200

    question bank created

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ question_bank = []
 for question in question_data:
     question_text = ["text"]
     question_answer = ["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
